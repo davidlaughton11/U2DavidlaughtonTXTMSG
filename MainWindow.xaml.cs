6d511fd9425b7e72ff06f7c5341e1e34404201ea
davidlaughton11 <47399940+davidlaughton11@users.noreply.github.com>
2019-03-25 18:49:23 -0400

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿//David laughton
+//March 29, 2019
+//Replace the txtmsg shortforms with real language.
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,36 +23,39 @@ namespace U2DavidLaughtonTXTMSG
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string intput;
-
         public MainWindow()
         {
             InitializeComponent();
         }
-        // If statements
-        // Spilt in textbox with string   
-        
-      
+
         private void BtnShortToLong_Click(object sender, RoutedEventArgs e)
         {
             //Make a textbox and get the text
-            //Use .replace to replace the text 
+            //Button intializes 
+
             string input = txtShortformInput.Text;
-            input = input.Replace("CU", "See you"); Console.WriteLine(input);
+             
+            //Use replace to replace the shortform 
+            //Console.WriteLine brings new line to output
+
+            input = input.Replace("CU", "see you"); Console.WriteLine(input);
             input = input.Replace(":-)", "I’m happy"); Console.WriteLine(input);
             input = input.Replace(":-(", "I’m unhappy"); Console.WriteLine(input);
-            input = input.Replace("", ""); Console.WriteLine(input);
-            input = input.Replace("", ""); Console.WriteLine(input);
-            input = input.Replace("", ""); Console.WriteLine(input);
-            input = input.Replace("", ""); Console.WriteLine(input);
-            input = input.Replace("", ""); Console.WriteLine(input);
-            input = input.Replace("", ""); Console.WriteLine(input);
-            input = input.Replace("", ""); Console.WriteLine(input);
-
-
+            input = input.Replace(";-)", "wink"); Console.WriteLine(input);
+            input = input.Replace(":-P", "stick out my tongue"); Console.WriteLine(input);
+            input = input.Replace("(˜.˜)", "sleepy"); Console.WriteLine(input);
+            input = input.Replace("TA", "totally awesome"); Console.WriteLine(input);
+            input = input.Replace("CCC", "Canadian Computing Competition"); Console.WriteLine(input);
+            input = input.Replace("CUZ", "because"); Console.WriteLine(input);
+            input = input.Replace("TY", "thank-you"); Console.WriteLine(input);
+            input = input.Replace("YW", "you’re welcome"); Console.WriteLine(input);
+            input = input.Replace("TTYL", "talk to you later"); Console.WriteLine(input);
+            
+            //Output to lael 
+            //Make output equal the new input after replaced
 
             string output = input;
-            txtOutput.Content = output;
+            txtOutput.Content = output;          
         }
     }
 }
